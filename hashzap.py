c237bb029cc770e5b8e8c3861c9fea400becf74d
Gabriel Silva <gnrgs@outlook.com>
2024-05-10 17:21:17 -0300

    refactor: Adição dos elementos do botões para a tela de popup e limpeza de tela ao clicar no botão do popup

--- hashzap.py
+++ hashzap.py
@@ -5,6 +5,7 @@
     # Campo de texto -> Escreva seu nome no chat
     #  Botão: Entrar no chat
         # Sumir com o título Hashzap
+        # Sumir botão 'Iniciar chat'
         # Fechar a janela (popup)
         # Carregar o chat 
             # As mensagens que já foram enviadas
@@ -15,9 +16,32 @@
 import flet as ft
 
 #criar a função principal do seu aplicativo 
-def main(page):
+def main(pagina):
     #criar todas as funcionalidades
-    pass
+    def entrar_chat(evento):
+        pagina.remove(titulo_pagina)
+        pagina.remove(botao_iniciar)
+        popup_solicitacao_informacoes.open = False
+        pagina.update()
+
+    def iniciar_chat(evento):
+        print('Iniciar chat')
+        pagina.dialog = popup_solicitacao_informacoes
+        popup_solicitacao_informacoes.open = True
+        pagina.update()
+
+    # cria o elemento
+    titulo_pagina = ft.Text('Hashzap')
+    titulo_popup = ft.Text('Bem vindo ao Hashzap')
+    campo_nome_usuario = ft.TextField(label='Escreva seu nome no chat')
+
+    botao_entrar_chat = ft.ElevatedButton('Entrar no chat', on_click=entrar_chat)
+    popup_solicitacao_informacoes = ft.AlertDialog(title=titulo_popup,content=campo_nome_usuario,actions=[botao_entrar_chat])
+    botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=iniciar_chat)
+    
+    # adiciona o elemento na página
+    pagina.add(titulo_pagina)
+    pagina.add(botao_iniciar)
 
 # rodar o aplicativo
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER)
