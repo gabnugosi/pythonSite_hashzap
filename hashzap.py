4c4db3f1cc63cc500474cd971c1b32321bc84b39
Gabriel Silva <gnrgs@outlook.com>
2024-05-10 13:25:02 -0300

    Commit inicial

---
+++ hashzap.py
@@ -0,0 +1,23 @@
+# Título: Hashzap
+# Botão de iniciar chat
+    # popup (janela na frente da tela)
+    # Título: Bem vindo ao Hashzap
+    # Campo de texto -> Escreva seu nome no chat
+    #  Botão: Entrar no chat
+        # Sumir com o título Hashzap
+        # Fechar a janela (popup)
+        # Carregar o chat 
+            # As mensagens que já foram enviadas
+            # Campo: Digite sua mensagem
+            # Botão de Enviar
+# pip install flet 
+
+import flet as ft
+
+#criar a função principal do seu aplicativo 
+def main(page):
+    #criar todas as funcionalidades
+    pass
+
+# rodar o aplicativo
+ft.app(main)
